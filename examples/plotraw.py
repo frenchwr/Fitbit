42ff6d4fcebb01ee06ce84c6da870ffc08258d84
Will French <william.r.french@gmail.com>
2014-11-02 08:01:48 -0600

    Updated python pacakges and plotting wrappers

--- examples/plotraw.py
+++ examples/plotraw.py
@@ -1,39 +1,48 @@
-import pandas as pd # 7/4/2014 -- installed version 0.14.0
 from util.getFilePath import getFilePath
 from util.readRawCSV import readRawCSV
 from util.wrappers import plotDataFrame
 
 dataFile = getFilePath("fitbit_export_20140710.csv")
 [activity,sleep] = readRawCSV(dataFile,["Activity","Sleep"])
-allData = pd.concat([activity,sleep])
+#allData = pd.concat([activity,sleep])
 
-# plotDataFrame tests:
-# kind can be line, bar, barh, area, scatter, hexbin, pie 
-# Note: Some of the plot types work with time series data only (time vs. var Y);
-#       This includes line, bar, barh, and area.
-# Note: Others plot types are designed to plot var X vs. var Y.
-#       This includes scatter, hexbin, and pie.
+# 11/02/2014
+# I spent a lot of time previously optimizing the layout of the axis and tick labels
+# but when I updated all my python pacakges several months later everything was 
+# broken...I'm just going to keep these plotting wrappers very minimal from now
+# on and update the aestetics if I really need to
 
-# First test bar plots (time series data only)
-plotDataFrame(sleep,y=['Minutes Asleep','Minutes Awake'],kind='bar',startdate='2014-06-03',enddate='2014-06-24',stacked=True)
-plotDataFrame(sleep,y='Number of Awakenings',kind='barh',startdate='2014-06-03',enddate='2014-06-24')
+plotDataFrame(activity) # default kind is line
+plotDataFrame(sleep,kind='bar',stacked=True)
+plotDataFrame(activity,kind='area')
+plotDataFrame(activity,x='Minutes Sedentary',y='Minutes Very Active',kind='hexbin')
+plotDataFrame(sleep,kind='hist',stacked=True)
+plotDataFrame(activity,kind='box')
+plotDataFrame(activity,y=['Minutes Very Active','Minutes Lightly Active'],kind='pie')
+plotDataFrame(sleep,x='Minutes Asleep',y='Minutes Awake',kind='scatter')
+#activity.plot()
+#plt.show()
+#sleep.plot()
+#plt.show()
+#plotDataFrame(sleep,y=['Minutes Asleep','Minutes Awake'],kind='bar',startdate='2014-06-03',enddate='2014-06-24',stacked=True)
+#plotDataFrame(sleep,y='Number of Awakenings',kind='barh',startdate='2014-06-03',enddate='2014-06-24')
 # This should fail; only allow barplots when x is time
 #plotDataFrame(sleep,x='Time in Bed',y=['Minutes Asleep','Minutes Awake','Number of Awakenings'],kind='bar',startdate='2014-03-01',enddate='2014-09-24')
 
 # Test scatter plots (does not support time series figures, so must provide xdata)
-plotDataFrame(sleep,x='Time in Bed',y=['Minutes Asleep','Minutes Awake','Number of Awakenings'],kind='scatter',startdate='2014-06-01',enddate='2014-06-24')
-plotDataFrame(activity,x='Minutes Sedentary',y=['Minutes Very Active','Minutes Lightly Active'],kind='scatter')
+#plotDataFrame(sleep,x='Time in Bed',y=['Minutes Asleep','Minutes Awake','Number of Awakenings'],kind='scatter',startdate='2014-06-01',enddate='2014-06-24')
+#plotDataFrame(activity,x='Minutes Sedentary',y=['Minutes Very Active','Minutes Lightly Active'],kind='scatter')
 
 # Test area plots (time series data)
-plotDataFrame(sleep,kind='area',startdate='2014-06-01',enddate='2014-06-24')
-plotDataFrame(activity,y=['Minutes Sedentary','Minutes Very Active','Minutes Lightly Active'],kind='area',stacked=False)
-plotDataFrame(activity,y='Steps',kind='area')
+#plotDataFrame(sleep,kind='area',startdate='2014-06-01',enddate='2014-06-24')
+#plotDataFrame(activity,y=['Minutes Sedentary','Minutes Very Active','Minutes Lightly Active'],kind='area',stacked=False)
+#plotDataFrame(activity,y='Steps',kind='area')
 
 # Test hexbin plotting (does not support time series figures, so must provide xdata)
-plotDataFrame(sleep,x='Time in Bed',y='Minutes Asleep',kind='hexbin',startdate='2014-03-01',enddate='2014-08-24')
-plotDataFrame(activity,x='Minutes Sedentary',y=['Minutes Lightly Active'],kind='hexbin')
+#plotDataFrame(sleep,x='Time in Bed',y='Minutes Asleep',kind='hexbin',startdate='2014-03-01',enddate='2014-08-24')
+#plotDataFrame(activity,x='Minutes Sedentary',y=['Minutes Lightly Active'],kind='hexbin')
 
 # Test simple line plots (time series data)
-plotDataFrame(sleep,kind='line',startdate='2014-06-01',enddate='2014-06-24')
-plotDataFrame(activity,y=['Minutes Sedentary','Minutes Very Active','Minutes Lightly Active'],kind='line')
-plotDataFrame(activity,y='Steps',kind='line')
+#plotDataFrame(sleep,kind='line',startdate='2014-06-01',enddate='2014-06-24')
+#plotDataFrame(activity,y=['Minutes Sedentary','Minutes Very Active','Minutes Lightly Active'],kind='line')
+#plotDataFrame(activity,y='Steps',kind='line')
--- util/wrappers.py
+++ util/wrappers.py
@@ -19,7 +19,7 @@ def definePlotLabels(ax,y,kind):
     
 
 # A wrapper for plotting a pandas dataframe
-def plotDataFrame(df,x=None,y=None,kind=None,startdate=None,enddate=None,stacked=False,gridsize=35):
+def plotDataFrame(df,x=None,y=None,kind=None,startdate=None,enddate=None,stacked=False,gridsize=35,bins=15):
     
     if type(x) is str:
         x = [x] # convert string to list
@@ -31,72 +31,91 @@ def plotDataFrame(df,x=None,y=None,kind=None,startdate=None,enddate=None,stacked
     if enddate is not None:
         df = df[:enddate]
     
-    fig, ax = plt.subplots()
-    colors = ['r', 'b', 'g', 'm', 'c', 'k']
+    # kind does not play nice when set to none, so need to define behavior for each plotting type
     
-    if kind is 'bar' or kind is 'barh':
+    if kind is 'bar' or kind is 'barh' or kind is 'area': df.plot(x=x,y=y,kind=kind,stacked=stacked)
+    elif kind is 'hexbin': df.plot(x=x,y=y,kind=kind,gridsize=gridsize)
+    elif kind is 'hist': df.plot(x=x,y=y,kind=kind,stacked=stacked,bins=bins)
+    elif kind is 'box': df.plot(x=x,y=y,kind=kind)
+    elif kind is 'scatter': df.plot(x=x,y=y,kind=kind)
+    elif kind is 'pie': df.plot(x=x,y=y,kind=kind)
+    else: df.plot(x=x,y=y)
+    plt.show()
         
-        if ( x is not None ):
-            print 'plot not generated: bar plot only supports time series plotting'
-            return
-        if ( y is None ):
-            df.plot(rot=25,kind=kind,stacked=stacked)
-        else:
-            df.plot(y=y,rot=25,kind=kind,stacked=stacked)
-            
-        definePlotLabels(ax,y,kind)
-        indices = arange(0,len(df.index),len(df.index)/5)
-        if kind is 'bar':
-            ax.set_xticks( indices )
-            ax.set_xticklabels( df.index[indices] )
-        else:
-            ax.set_yticks( indices )
-            ax.set_yticklabels( df.index[indices] )
-    elif kind is 'scatter':
-        for i,yi in enumerate(y):
-            if i is 0:
-                df.plot(x=x,y=yi,rot=25,kind=kind,ax=ax,color=colors[i],label=yi)
-            else:
-                df.plot(x=x,y=yi,rot=25,kind=kind,ax=ax,color=colors[i],label=yi)
-        ax.set_xlabel(x[0],fontsize=24)  
-        definePlotLabels(ax,y,kind)
-    elif kind is 'area':
-        
-        if ( x is not None ):
-            print 'plot not generated: area plot only supports time series plotting'
-            return
-        if ( y is None ):
-            df.plot(rot=25,ax=ax,kind=kind,stacked=stacked)
-        else:
-            df.plot(y=y,rot=25,kind=kind,stacked=stacked)
-            definePlotLabels(ax,y,kind)
-            
-        
-    elif kind is 'hexbin':
-        
-        if ( len(y) > 1 ):
-            print 'Only one y value can be plotted with hexbin'
-            return
-        
-        df.plot(x=x,y=y,rot=25,ax=ax,kind=kind,gridsize=gridsize)
-        ax.set_xlabel(x[0],fontsize=24)  
-        definePlotLabels(ax,y,kind)
-            
-    elif kind is 'pie':
-        print 'pie plots currently not supported'
-        return
-    elif kind is 'line' or kind is None:
-        if ( x is not None ):
-            print 'plot not generated: area plot only supports time series plotting'
-            return
-        if ( y is None ):
-            df.plot(rot=25,ax=ax,kind=kind)
-        else:
-            df.plot(y=y,rot=25,kind=kind)
-            definePlotLabels(ax,y,kind)
+    return
     
-    else:
-        print 'unknown plot kind'
-        return
+    #fig, ax = plt.subplots()
+    #colors = ['r', 'b', 'g', 'm', 'c', 'k']
     
-    fig.show()
+    #if kind is 'bar' or kind is 'barh':
+        
+    #    if ( x is not None ):
+    #        print 'plot not generated: bar plot only supports time series plotting'
+    #        return
+    #    if ( y is None ):
+    #        df.plot(rot=25,kind=kind,stacked=stacked)
+    #    else:
+    #        print "Hello!"
+    #        #df.plot(y=y,rot=25,kind=kind,stacked=stacked)
+    #        #df.plot(y=y,kind=kind)
+    #        df.plot(y='Minutes Asleep',kind='bar')
+            
+        #definePlotLabels(ax,y,kind)
+    #    indices = arange(0,len(df.index),len(df.index)/5)
+    #    print indices
+    #    if kind is 'bar':
+    #        print "Just passing through..."
+    #        #ax.set_xticks( indices )
+    #        #ax.set_xticklabels( df.index[indices] )
+    #        pass
+    #    else:
+    #        ax.set_yticks( indices )
+    #        ax.set_yticklabels( df.index[indices] )
+    #elif kind is 'scatter':
+    #    for i,yi in enumerate(y):
+    #        if i is 0:
+    #            df.plot(x=x,y=yi,rot=25,kind=kind,ax=ax,color=colors[i],label=yi)
+    #        else:
+    #            df.plot(x=x,y=yi,rot=25,kind=kind,ax=ax,color=colors[i],label=yi)
+    #    ax.set_xlabel(x[0],fontsize=24)  
+    #    definePlotLabels(ax,y,kind)
+    #elif kind is 'area':
+    #    
+    #    if ( x is not None ):
+    #        print 'plot not generated: area plot only supports time series plotting'
+    #        return
+    #    if ( y is None ):
+    #        df.plot(rot=25,ax=ax,kind=kind,stacked=stacked)
+    #    else:
+    #        df.plot(y=y,rot=25,kind=kind,stacked=stacked)
+    #        definePlotLabels(ax,y,kind)
+    #        
+    #    
+    #elif kind is 'hexbin':
+    #    
+    #    if ( len(y) > 1 ):
+    #        print 'Only one y value can be plotted with hexbin'
+    #        return
+    #    
+    #    df.plot(x=x,y=y,rot=25,ax=ax,kind=kind,gridsize=gridsize)
+    #    ax.set_xlabel(x[0],fontsize=24)  
+    #    definePlotLabels(ax,y,kind)
+    #        
+    #elif kind is 'pie':
+    #    print 'pie plots currently not supported'
+    #    return
+    #elif kind is 'line' or kind is None:
+    #    if ( x is not None ):
+    #        print 'plot not generated: area plot only supports time series plotting'
+    #        return
+    #    if ( y is None ):
+    #        df.plot(rot=25,ax=ax,kind=kind)
+    #    else:
+    #        df.plot(y=y,rot=25,kind=kind)
+    #        definePlotLabels(ax,y,kind)
+    #
+    #else:
+    #    print 'unknown plot kind'
+    #    return
+    #
+    #fig.show()
